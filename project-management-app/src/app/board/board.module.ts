6db93239c63438eb71f923b08aefeb458042c7f9
Your Name <you@example.com>
2022-04-25 17:01:35 +0300

    feat: add error-page, boards-page, admin-page components

--- project-management-app/src/app/board/board.module.ts
+++ project-management-app/src/app/board/board.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { MaterialModule } from '../material/material.module';
+import { BoardsPageComponent } from './boards-page/boards-page.component';
+import { BoardRoutingModule } from './board-routing.module';
 
 @NgModule({
-  declarations: [MainPageComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    MainPageComponent,
+    AdminPageComponent,
+    ErrorPageComponent,
+    BoardsPageComponent,
+  ],
+  imports: [CommonModule, BoardRoutingModule, MaterialModule],
   exports: [MainPageComponent],
 })
 export class BoardModule {}
---
+++ project-management-app/src/app/board/boards-page/boards-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-page',
+  templateUrl: './boards-page.component.html',
+  styleUrls: ['./boards-page.component.scss']
+})
+export class BoardsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
