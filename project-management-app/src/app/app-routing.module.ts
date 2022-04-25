ca4ea42c283686397b2a0f9764980710504bdc85
Your Name <you@example.com>
2022-04-25 23:05:38 +0300

    :refactor: add routing

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './board/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      // eslint-disable-next-line implicit-arrow-linebreak
+      import('./board/board.module').then((m) => m.BoardModule),
+  },
+  { path: '**', component: ErrorPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project-management-app/src/app/board/board-routing.module.ts
+++ project-management-app/src/app/board/board-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-
 import { BoardsPageComponent } from './boards-page/boards-page.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'boards', component: BoardsPageComponent },
-  { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
