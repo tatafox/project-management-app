5ea6a7c889d378bd4348a5fa587d106253ea176e
Your Name <you@example.com>
2022-04-26 13:31:45 +0300

    :refactor: create service for auth

--- project-management-app/src/app/auth/components/sign-up/sign-up.component.ts
+++ project-management-app/src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSignUp } from 'src/app/shared/interfaces';
+import { UserAuthServiceService } from '../../services/user-auth-service.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
+  providers: [UserAuthServiceService],
 })
 export class SignUpComponent implements OnInit {
+  public titles = {
+    describe: "You have to sign up, if you haven't got an account",
+    headTitle: 'Sign Up',
+    name: 'Name',
+    login: 'Login',
+    password: 'Password',
+  };
 
-  constructor() { }
+  public user: IUserSignUp = {
+    name: 'Vasya',
+    login: 'user001',
+    password: 'userpass@123',
+  };
 
-  ngOnInit(): void {
+  receivedUser: IUserSignUp | undefined; // полученный пользователь
+
+  done: boolean = false;
+
+  constructor(private authService: UserAuthServiceService) {}
+
+  ngOnInit(): void {}
+
+  submit(user: IUserSignUp) {
+    this.authService.postDataUser(user).subscribe(
+      (data: any) => {
+        this.receivedUser = data;
+        this.done = true;
+      },
+      (error) => console.log(error),
+    );
   }
-
 }
